b34c3374b44ebf7d3450e775cb1448c477eeb977
DarcinBig <biganirodarcin@gmail.com>
2025-06-20 21:18:15 +0200

    Add routing for course-related pages and implement AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,12 @@
-import { createContext } from "react";
-
-export const AppContext = createContext()
+import React from "react";
+import { AppContext } from "./Context";
 
 export const AppContextProvider = (props) => {
+  const value = {};
 
-    const value = {}
-
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>
+        {props.children}
+    </AppContext.Provider>
+  );
+};
