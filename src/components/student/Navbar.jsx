0660f4da4fc78d2fe3c977bfc80867f5a522f90a
DarcinBig <biganirodarcin@gmail.com>
2025-06-20 22:41:40 +0200

    Add Navbar component and conditionally render it based on route

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  
+  const isCourseListPage = location.pathname.includes('/course-list')
+
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+      <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+        <div className='flex items-center gap-5'>
+          <button>Become Educator</button>
+          | <Link to='/my-enrollments'>My Enrollments</Link>
+        </div>
+        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+      </div>
+      {/* For phone Screens */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+        <div>
+          <button>Become Educator</button>
+          | <Link to='/my-enrollments'>My Enrollments</Link>
+        </div>
+        <button><img src={assets.user_icon} alt="user icon" /></button>
+      </div>
     </div>
   )
 }
