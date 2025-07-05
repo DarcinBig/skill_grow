d2266d9385cb86ee1ac7f9b4b252ba71afe16417
DarcinBig <biganirodarcin@gmail.com>
2025-07-05 18:12:17 +0200

    `Added express.json() middleware to specific routes`

--- server/server.js
+++ server/server.js
@@ -18,15 +18,14 @@ await connectCloudinary()
 
 // Middlewares
 app.use(cors())
-app.use(express.json())
 app.use(clerkMiddleware())
 
 // Routes (API Endpoints)
 app.get('/', (req, res) => res.send('API is Working!'))
 app.post('/clerk', express.json(), clerkWebhooks)
-app.use('/api/educator', educatorRouter)
-app.use('/api/course', courseRouter)
-app.use('/api/user', userRouter)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 // Port
